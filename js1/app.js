e988aeab9c8b64237e625cbb770f3700362c6ea1
Saule <134058561+Saule-02@users.noreply.github.com>
2023-05-28 15:26:15 +0600

    jklhj

---
+++ js1/app.js
@@ -0,0 +1,68 @@
+// console.log('HELLO WORLD'); 
+// ' ' 
+// " " 
+// ` ` 
+// типы данных
+// 1. number
+// var -- variable
+var number1 = 100
+var age = 18
+var usd = 88.45
+var temp = -10 
+
+// 2. string 
+var userName = 'Saule' 
+
+// 3. boolean (true / false)
+var hasCar = false 
+var isTrue = 8 > 4  // true
+
+// console.log(isTrue);
+
+// 4. undefined
+var age = undefined
+var  empty
+// console.log(empty);
+
+// 5. null 
+var nullExpl = null 
+// console.log(nullExpl);
+
+// 6. Symbol
+var sym = Symbol()
+// console.log(sym);
+
+// 7. Object { }   [ ]
+var car = {
+    //key : value
+    //ключ : значение
+
+    model: 'BMW',
+    color: 'black',
+    price: 200000,
+    year: 2018
+}
+// console.log(car);
+
+// 8. begInt 
+var bInt  = 4n
+
+var movies = [ 'Avatar', 'Chas Pik', 'Jon wik', 'Forsaj 10' ]
+
+
+var computer = {
+brand: 'Lenovo',
+color: 'gray',
+tip: 'thin',
+state: 'new',
+year: 2020,
+price: 50_000,
+numberOfCores: 6, 
+ОС: 'Windows',
+MemoryType: 'DDR4',
+CPU: 'Intel Core i5',
+earphone: null,
+mouse: true,
+}
+
+console.log(computer);
